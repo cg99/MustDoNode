cd34bd19de39c02ec150464bac3e80f02116cb18
cg99 <darkinfinitespace@gmail.com>
2024-07-10 14:30:14 +1000

    import dotenv

--- src/app.js
+++ src/app.js
@@ -1,19 +1,22 @@
 // src/app.js
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
 
+
 // CORS configuration
 const corsOptions = {
-    origin: `${process.env.CLIENT_URL}}`,
+    origin: process.env.CLIENT_URL,
     optionsSuccessStatus: 200,
-    credentials: true
-  };
-  
+};
+
 
 app.use(cors(corsOptions));
 app.use(express.json());
+
 
 app.use('/api/tasks', require('./routes/tasks'));
 app.use('/api/quotes', require('./routes/quotes'));
