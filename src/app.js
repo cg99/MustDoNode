448519ca19c6c89fc1a4dda623c6b5b52b67ad2c
cg99 <darkinfinitespace@gmail.com>
2024-07-10 14:01:09 +1000

    client url

--- src/app.js
+++ src/app.js
@@ -4,7 +4,14 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+// CORS configuration
+const corsOptions = {
+    origin: [`${process.env.CLIENT_URL}}`],
+    optionsSuccessStatus: 200
+  };
+  
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/api/tasks', require('./routes/tasks'));
