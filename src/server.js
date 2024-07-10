602a91b8532b3fe960f9222d8765aac3142d5031
cg99 <darkinfinitespace@gmail.com>
2024-07-10 15:26:12 +1000

    vercel for node

--- src/server.js
+++ src/server.js
@@ -8,4 +8,6 @@ connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
+}).catch((error) => {
+  console.error('Failed to connect to the database:', error);
 });
