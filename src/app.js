8868972b77714b591845e0ad71792d802bbfda9f
cg99 <darkinfinitespace@gmail.com>
2024-07-10 14:56:08 +1000

    vercel cors

--- src/app.js
+++ src/app.js
@@ -23,7 +23,6 @@ app.options('*', cors(corsOptions));
 
 app.use(express.json());
 
-
 app.use('/api/tasks', require('./routes/tasks'));
 app.use('/api/quotes', require('./routes/quotes'));
 app.use('/api/auth', require('./routes/auth')); // Register the auth routes
